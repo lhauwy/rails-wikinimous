2c65eb56b516eaf9963f5ac3d8f9f8d0f3738c3b
laurent hauwy <hauwylaurent@gmail.com>
2018-11-20 16:07:19 +0100

    add article controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.delete_all
+
+10.times do
+  Article.create!(
+    title: Faker::TheFreshPrinceOfBelAir.character,
+    content: Faker::TheFreshPrinceOfBelAir.quote
+  )
+end
